2f0e0bd1a1739bb93ade3d741a39ccc5c87976c4
matanlieber <matan99199@gmail.com>
2017-12-30 15:47:55 +0200

    corrected script

--- tracks/getListenersFromLastFM.py
+++ tracks/getListenersFromLastFM.py
@@ -23,32 +23,26 @@ def main():
     with open(INPUT_FILE, 'r') as fin, open(OUTPUT_FILE, 'w') as fout:
         reader = csv.reader(fin, lineterminator='\n')
         writer = csv.writer(fout, lineterminator='\n')
-        for row in reader:
-            try:                
-                track_title = row[track_title_col_index]
-                artist_name = row[artist_name_col_index]
-                print(artist_name + ": " + track_title)
-                
-                #search in lyrics API's:
-                # lyrics.ovh:
-                try:
-                    trackData = (requests.get(createRequestURI(track_title,artist_name))).json()
-                    track = trackData["results"]["trackmatches"]["track"]
-                    if len(track) == 0  or "listeners" not in track[0]:
-                        print(artist_name+" "+track_title+" not found")
-                        writer.writerow(row + ["NULL"])                        
-                    else:             
-                                          
-                        writer.writerow(row + [track[0]["listeners"]])
-                        continue
+        for row in reader:                          
+            track_title = row[track_title_col_index]
+            artist_name = row[artist_name_col_index]
+            print(artist_name + ": " + track_title)
+            
+            #search in lyrics API's:
+            # lyrics.ovh:
+            try:
+                trackData = (requests.get(createRequestURI(track_title,artist_name))).json()
+                track = trackData["results"]["trackmatches"]["track"]
+                if len(track) == 0  or "listeners" not in track[0]:
+                    print(artist_name+" "+track_title+" not found")
+                    writer.writerow(row + ["NULL"])                        
+                else:                                          
+                    writer.writerow(row + [track[0]["listeners"]])
+                    continue
 
-                except requests.exceptions.RequestException:
-                    print("except")
-                    writer.writerow(row + ["NULL"])
-                    
-            except:
-                print("encoding error")
-                writer.writerow(row + ["NULL"])
+            except requests.exceptions.RequestException:
+                print("except")
+                writer.writerow(row + ["NULL"])       
             
 
 main()
