11dde42f48ad3fc2127e2a0e9954ea350f09c7b7
itaiyifrach <itai.yifrach@gmail.com>
2018-01-11 14:07:20 +0200

    [UPDATE] database design

--- InsertArtists.py
+++ InsertArtists.py
@@ -4,16 +4,6 @@ from connectionInfo import *
 
 INPUT_FILE = "artists\\artists.csv"
 
-def getForeignKeyFromTable(query, value):
-    # execute the SQL query using execute() method.
-    cursor.execute(query, [value])
-    # fetch all of the rows from the query
-    data = cursor.fetchall()
-    if (data== ()):     # not found
-        return None
-    else:
-        return data[0][0]   
-
 # Open database connection
 db = MySQLdb.connect(host=SERVER_NAME, port=SERVER_PORT, user=DB_USERNAME, passwd=DB_PASSWORD, db=DB_NAME)
 
@@ -34,20 +24,19 @@ with open(INPUT_FILE, 'r') as fin:
         artist_genre = row[3]
         artist_playcount = row[4]
         artist_listeners = row[5]
-        
+
+        # set null values
+        if (artist_genre == "N\\A"):
+            artist_genre = None
 
         # define sql queries
-        add_artist = """INSERT INTO Artist (artist_id, name, genre_id, playcount, listeners)
+        add_artist = """INSERT INTO Artist (artist_id, name, genre, playcount, listeners)
                         VALUES (%s,%s,%s,%s,%s)"""
-        get_genre_id = "SELECT genre_id from Genre WHERE genre = %s"
-
-        # getting the foreign keys from genre table
-        genre = getForeignKeyFromTable(get_genre_id, artist_genre)
 
         # inserting the artist
         try:
             # inserting 0 for artist_id to use sql auto increment
-            cursor.execute(add_artist, (0,artist_name,genre,artist_playcount,artist_listeners))
+            cursor.execute(add_artist, (0,artist_name,artist_genre,artist_playcount,artist_listeners))
             db.commit()
         except Exception as e:
             print("error")
