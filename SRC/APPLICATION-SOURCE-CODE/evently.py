803b106029b74b6d0a07a742275d2f1a66029cff
mayacahana <maya.cahana24@gmail.com>
2018-01-11 18:13:47 +0200

    // fix

--- SRC/APPLICATION-SOURCE-CODE/evently.py
+++ SRC/APPLICATION-SOURCE-CODE/evently.py
@@ -6,16 +6,18 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 # Create the application instance
 app = Flask(__name__)
 # MySQL configurations
-mysql = mydb()
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
-mysql.init_app(app)
+# =app.config['MYSQL_DATABASE_USER'] = ''
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = ''
+# app.config['MYSQL_DATABASE_HOST'] = ''
+# mysql.init_app(app)
 
 @app.route('/events', methods=['GET'])
 def showEvents():
     return render_template('eventsList.html')
 
+@app.route('/')
 def main():
     return render_template('homepage.html')
+if (__name__ == '__main__'):
+    app.run(port=8888, host = "0.0.0.0", debug=True)
