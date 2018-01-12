9f5eb3a5773ac4fa89aae1261c3f8a171bc52612
matanlieber <matan99199@gmail.com>
2018-01-12 20:12:03 +0200

    [UPDATE] fresh artists query hamastan shel Maya

--- Querys/query_fresh_artists.sql
+++ Querys/query_fresh_artists.sql
@@ -1,31 +1,38 @@
 # find event
-# FRESH ARTISTS - Find events of popular artists (sorted by playcount)
-# in the 2 months before @date
-# that dont perform more than @times times in the 30 days before the show 
+# FRESH ARTISTS - Find events of popular artists (sorted by listeners)
+# that dont perform more than @times times in the 30 days before the show
+# in the the 2 months from the date @date
 
-CREATE OR REPLACE VIEW Events_for_artists AS
-SELECT  A.artist_id as artist_id, A.name as artist_name,
-		A.genre as genre, A.listeners as listeners, A.playcount as playcount,
-        E.event_id as event_id, E.date as event_date, E.country_id
-FROM Event AS E INNER JOIN Artist AS A ON E.artist_id = A.artist_id
-WHERE CURRENT_DATE() <= E.date;
-
+#getter functions for using params in views
 DROP FUNCTION IF EXISTS getDate;
-CREATE FUNCTION getDate() RETURNS INTEGER DETERMINISTIC RETURN @date;
-CREATE OR REPLACE VIEW latest_events_60 AS
-SELECT E.event_id as event_id, E.date as event_date, A.artist_id as artist_id, A.name as artist_name
-FROM event as E INNER JOIN artist as A ON E.artist_id = A.artist_id
-WHERE DATEDIFF(E.event_date, getDate())>=0 and DATEDIFF(E.event_date, getDate())<=60;
+CREATE FUNCTION getDate() RETURNS DATE DETERMINISTIC RETURN @date;
 
-CREATE OR REPLACE VIEW latest_events_90 AS
-SELECT E.event_id as event_id, E.date as event_date, A.artist_id as artist_id, A.name as artist_name
-FROM event as E INNER JOIN artist as A ON E.artist_id = A.artist_id
-WHERE DATEDIFF(E.date, getDate())>=0 and DATEDIFF(E.date, getDate())<=90;
+DROP FUNCTION IF EXISTS getTimes;
+CREATE FUNCTION getTimes() RETURNS INTEGER DETERMINISTIC RETURN @times;
 
-select *
-from latest_events_60 AS L
-where EXISTS (SELECT * 
-			  FROM latest_events_90 as L2
-              WHERE L.artist_id = L2.artist_id AND DATEDIFF(L.event_date , L2.event_date)
-              GROUP BY L.artist_id
-              HAVING COUNT(L2.event_id<=@times))
+CREATE OR REPLACE VIEW events_60 AS
+	# all the events + 60 from current date
+	SELECT *
+	FROM event as E
+	WHERE DATEDIFF(E.date,getDate()) <= 60;
+
+CREATE OR REPLACE VIEW relevant_events AS
+  # all the relevant events as defined above
+  SELECT *
+  FROM events_60 as E2
+  WHERE EXISTS (
+				SELECT E3.artist_id
+				FROM event as E3
+				WHERE DATEDIFF(E2.date,E3.date) <= 30 AND 
+					  DATEDIFF(E2.date,E3.date) > 0   AND 
+					  E2.artist_id = E3.artist_id
+				HAVING COUNT(E3.event_id) < getTimes());
+
+SET @date = "2018-03-01";
+SET @times = 1;
+# the query			
+SELECT A.name, D.sale_date, D.date, D.venue, C.country, C2.city
+FROM relevant_events AS D INNER JOIN Artist AS A ON D.artist_id = A.artist_id
+     INNER JOIN Country AS C ON C.country_id = D.country_id 
+	 INNER JOIN City AS C2 ON C2.city_id = D.city_id  
+ORDER BY A.playcount DESC                          
