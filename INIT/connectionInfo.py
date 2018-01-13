2030488cd916c94dd7403cb428e28745b2846f1a
matanlieber <matan99199@gmail.com>
2018-01-13 16:58:37 +0200

    removed obsolete data

--- INIT/connectionInfo.py
+++ INIT/connectionInfo.py
@@ -1,8 +1,8 @@
 import os
 
 SERVER_NAME = "127.0.0.1"
-SERVER_PORT = 3306
-DB_USERNAME = "root"
-DB_PASSWORD = "Mc240195"
+SERVER_PORT = 3305
+DB_USERNAME = "DbMysql11"
+DB_PASSWORD = "DbMysql11"
 DB_NAME = "DbMysql11"
 PATH_ROOT = os.path.dirname(os.getcwd())
