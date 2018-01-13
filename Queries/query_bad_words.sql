4732001df80801591e1ffc3d008e860e2c1121d5
itaiyifrach <itai.yifrach@gmail.com>
2018-01-13 15:23:49 +0200

    [UPDATE] optimized bad words query

--- Queries/query_bad_words.sql
+++ Queries/query_bad_words.sql
@@ -1,69 +1,29 @@
-
-
-SET @artistId = 1;
-SET @badWord = "fuck fucking awesome";
-#returns the genre of the artist
-CREATE OR REPLACE VIEW Artist_Genre AS
-select A.genre as genre
-from Artist as A
-where A.artist_id = getArtistId();
-
-#returns songs of artists from genre
-CREATE OR REPLACE VIEW Songs_Genre AS
-select  A.artist_id as artist_id, A.name, T.track_id as track_id
-from Artist as A INNER JOIN Track as T ON A.artist_id=T.artist_id
-where A.genre = (select *
-				 from Artist_Genre);
+# return total tracks which has lyrics of input artist's genre which 
+DROP FUNCTION IF EXISTS getArtistID;
+CREATE FUNCTION getArtistID() RETURNS SMALLINT(5) RETURN @artist_id;
+SET @artist_id = 1;
+CREATE OR REPLACE VIEW ALL_SONGS AS
+SELECT  A.artist_id, A.name, T.track_id, COUNT(T.track_id) AS num_of_songs
+FROM Track AS T INNER JOIN Artist AS A ON T.artist_id = A.artist_id
+	JOIN Lyrics AS L ON T.track_id = L.track_id
+WHERE A.genre = (SELECT Artist.genre FROM Artist WHERE artist_id = getArtistID())
+		AND L.lyrics IS NOT NULL
+GROUP BY A.artist_id;
                  
-#returns lyrics of songs in genre
-DROP VIEW IF EXISTS lyrics_genre;
-CREATE VIEW lyrics_genre AS
-select distinct Artist.name, Artist.artist_id, Track.title, Track.track_id,  Artist.genre , Lyrics.lyrics 
-from Artist INNER JOIN Songs_Genre ON Artist.artist_id = Songs_Genre.artist_id 
-INNER JOIN Track ON track.artist_id = Songs_Genre.artist_id 
-INNER JOIN Lyrics ON Lyrics.track_id = Track.track_id
-where lyrics.lyrics IS NOT NULL ;    
- 
-#returns songs of artists from genre
-DROP view if exists ALL_SONGS;
-CREATE VIEW ALL_SONGS AS                
-select artist_id, count(track_id) as num_of_songs
-from lyrics_genre
-group by artist_id;
-                 
-SELECT 
-    Track.title
-FROM
-    Artist
-        INNER JOIN
-    Track ON Artist.artist_id = Track.artist_id
-WHERE
-    Artist.artist_id = (SELECT 
-            M.artist_id
-        FROM
-            (SELECT 
-                BAD_LYRICS.artist_id,
-                    COUNT(BAD_LYRICS.track_id) / ALL_SONGS.num_of_songs * 100 AS percentOfBadSongs
-            FROM
-                (SELECT 
-                Artist.name AS artist_name,
-                    Artist.artist_id AS artist_id,
-                    Track.track_id AS track_id,
-                    lyrics.lyrics AS lyrics
-            FROM
-                Artist
-            INNER JOIN Track ON Artist.artist_id = Track.artist_id
-            INNER JOIN lyrics ON lyrics.track_id = track.track_id
-            WHERE
-                Artist.genre = (SELECT 
-                        *
-                    FROM
-                        Artist_Genre)
-                    AND MATCH (lyrics) AGAINST (@badWord IN BOOLEAN MODE)) AS BAD_LYRICS, ALL_SONGS
-            WHERE
-                ALL_SONGS.artist_id = BAD_LYRICS.artist_id
-            GROUP BY BAD_LYRICS.artist_id
-            ORDER BY percentOfBadSongs
-            LIMIT 1) AS M
-        ORDER BY track.listeners
-        LIMIT 20);
+SET @badWord1 = "fuck fucking awesome";
+
+SELECT Track.title
+FROM Artist INNER JOIN Track ON Artist.artist_id = Track.artist_id
+WHERE Artist.artist_id = (SELECT M.artist_id
+						  FROM (
+								SELECT BAD_LYRICS.artist_id,  COUNT(BAD_LYRICS.track_id)/ALL_SONGS.num_of_songs*100 AS percentOfBadSongs
+								FROM (SELECT Artist.artist_id as artist_id, Track.track_id AS track_id
+									  FROM Artist INNER JOIN Track ON Artist.artist_id = Track.artist_id INNER JOIN lyrics ON lyrics.track_id = track.track_id
+									   WHERE MATCH (lyrics) AGAINST (@badWord1 IN BOOLEAN MODE)) AS BAD_LYRICS , ALL_SONGS
+								WHERE ALL_SONGS.artist_id = BAD_LYRICS.artist_id
+								GROUP BY BAD_LYRICS.artist_id
+								ORDER BY percentOfBadSongs 
+								LIMIT 1) AS M
+
+						  ORDER BY track.listeners
+						  LIMIT 20);
