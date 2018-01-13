22f4e927d0f66c92b2077572dc151d438a1eba52
mayacahana <maya.cahana24@gmail.com>
2018-01-13 16:30:23 +0200

    matan ya tumtummmm

--- INIT/stored_procedures.sql
+++ INIT/stored_procedures.sql
@@ -9,7 +9,7 @@ DELIMITER //
 # by your favorite genre (inGenre) in specific location
 
 DROP PROCEDURE IF EXISTS top_artists//
-CREATE PROCEDURE top_artists(IN inGenre VARCHAR(45), IN numListeners INT, IN numSongs INT)
+CREATE PROCEDURE top_artists(IN inGenre VARCHAR(45), IN numListeners INT, IN numSongs INT, IN country_name VARCHAR(45))
 BEGIN
 SELECT A.artist_id as artist_id, artist_name, sale_date, event_date, country, city, venue, description
 FROM	(SELECT Artist.artist_id AS artist_id
@@ -20,7 +20,7 @@ FROM	(SELECT Artist.artist_id AS artist_id
         Artists_With_Future_Events AS E ON A.artist_id = E.artist_id
         INNER JOIN Country AS C ON E.country_id = C.country_id
 		INNER JOIN City ON E.city_id = City.city_id
-WHERE C.country = "United Kingdom";
+WHERE C.country = country_name;
 END//
 
 #query_fresh_artists
