687926de89fb2e6d61be02c8119d399e49c3c1dc
mayacahana <maya.cahana24@gmail.com>
2018-01-18 01:50:39 +0200

    [FIXED]

--- SRC/APPLICATION-SOURCE-CODE/static/myjavascript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#datepicker').datepicker({
-        autoclose: true,
-            format: 'yyyy-mm-dd',
-            todayHighlight: true,
-            endDate: '+0d',
-            startDate: '0d'
-     });
-});
